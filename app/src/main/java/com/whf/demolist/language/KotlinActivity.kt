e6dfa558512e2d10daf594e43e3406781e4dbaaf
wanghaofei <292265806@qq.com>
2019-04-27 11:57:29 +0800

    Kotlin测试

--- app/src/main/java/com/whf/demolist/language/KotlinActivity.kt
+++ app/src/main/java/com/whf/demolist/language/KotlinActivity.kt
@@ -2,14 +2,14 @@ package com.whf.demolist.language
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.whf.demolist.R
+import com.whf.demolist.language.kt.testCollection
 
 import com.whf.demolist.language.kt.tag as TAG
 
-
 //包级函数不在同一个包也需要导包
 import com.whf.demolist.language.kt.testFor
+import com.whf.demolist.language.kt.testTry
 
 //不需要 findViewById 的导包
 import kotlinx.android.synthetic.main.activity_kotlin.*
@@ -21,8 +21,16 @@ class KotlinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
 
-        package_method.setOnClickListener {
+        package_method_for.setOnClickListener {
             testFor()
+        }
+
+        package_method_try.setOnClickListener {
+            testTry()
+        }
+
+        package_method_collection.setOnClickListener{
+            testCollection()
         }
     }
 }
--- app/src/main/java/com/whf/demolist/language/kt/PackageClass.kt
+++ app/src/main/java/com/whf/demolist/language/kt/PackageClass.kt
@@ -1,15 +1,17 @@
 package com.whf.demolist.language.kt
 
 import android.util.Log
+import java.lang.Exception
 
+/**
+ * 顶层函数、属性，在包外访问的时候需要导包
+ */
 const val TAG = "KT_Test_"
 
-//给Any类扩展属性
+//给Any类扩展属性，实际只是提供了一个getTag的方法
 val Any.tag: String
     get() = TAG + this.javaClass.simpleName
 
-
-//包级函数测试，在同一个包里面不需要导包，否则也需要导包
 fun testFor() {
     //区间是左闭右闭
     for (i in 1..3) {
@@ -17,29 +19,29 @@ fun testFor() {
     }
 
     //区间为左闭右开
-    for (j in 1 until 3){
+    for (j in 1 until 3) {
         Log.d(TAG, "test for j: $j")
     }
 
     //步长为2，输出为1 3 5 7 9
-    for (k in 1..6 step 2){
+    for (k in 1..6 step 2) {
         Log.d(TAG, "test for k: $k")
     }
 
     //遍历list
-    val list = arrayListOf("a","b","c")
-    for ((index,value) in list.withIndex()){
+    val list = arrayListOf("a", "b", "c")
+    for ((index, value) in list.withIndex()) {
         Log.d(TAG, "$index = $value")
     }
 
     //创建map
-    val maps = HashMap<Int,String>()
+    val maps = HashMap<Int, String>()
     maps[0] = "a"
     maps[1] = "b"
     maps[2] = "c"
 
     //遍历map
-    for ((key,value) in maps){
+    for ((key, value) in maps) {
         Log.d(TAG, "test for map key = $key , value = $value")
     }
 }
@@ -52,12 +54,50 @@ fun testWhen(age: Int): String =
             else -> "骚年"
         }
 
-fun testIn(){
+fun testIn() {
 
     3 in 1..5
 
-    "a" in arrayListOf("a","b","c")
+    "a" in arrayListOf("a", "b", "c")
 
-    "b" !in arrayListOf("a","b","c")
+    "b" !in arrayListOf("a", "b", "c")
 }
 
+
+fun testTry() {
+    Log.d(TAG, "start test try!")
+    val number: Int = try {
+        Integer.parseInt("abc")
+
+    } catch (e: Exception) {
+        //return的是整个testTry方法
+        //return
+        1
+    }
+    Log.d(TAG, "number is $number")
+}
+
+
+fun testCollection() {
+
+
+    val set = hashSetOf(1, 2, 3)
+    Log.d(TAG, "hashSetOf class = ${set.javaClass.simpleName} ")
+
+    //Kotlin针对集合的扩展方法
+    Log.d(TAG, set.joinToString(prefix = "[", postfix = "]"))
+    val maxValue = set.max()
+    val last = set.last()
+
+    val array = arrayOf("b","c","d")
+    //可变参数中如果是数组，需要用展开运算符将数组展开
+    val list = listOf("a",*array)
+
+    val arrayList = arrayListOf(1, 2, 3)
+    Log.d(TAG, "arrayListOf class = ${arrayList.javaClass.simpleName} ")
+
+    //中缀符号调用 to 函数，原为：1.to("a")返回Pair对象
+    val hashMap = hashMapOf(1 to "a", 2 to "b", 3 to "c")
+    Log.d(TAG, "hashMapOf class = ${hashMap.javaClass.simpleName} ")
+
+}
