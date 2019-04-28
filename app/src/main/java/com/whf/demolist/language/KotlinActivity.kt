337b0e6d602dfb43f4b6ad50fa717f5a2fbe7c65
wanghaofei <292265806@qq.com>
2019-04-28 13:49:19 +0800

    Kotlin协程

--- app/src/main/java/com/whf/demolist/language/KotlinActivity.kt
+++ app/src/main/java/com/whf/demolist/language/KotlinActivity.kt
@@ -2,6 +2,7 @@ package com.whf.demolist.language
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.whf.demolist.R
 import com.whf.demolist.language.kt.testCollection
 
@@ -13,6 +14,10 @@ import com.whf.demolist.language.kt.testTry
 
 //不需要 findViewById 的导包
 import kotlinx.android.synthetic.main.activity_kotlin.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 
 class KotlinActivity : AppCompatActivity() {
@@ -29,9 +34,40 @@ class KotlinActivity : AppCompatActivity() {
             testTry()
         }
 
-        package_method_collection.setOnClickListener{
+        package_method_collection.setOnClickListener {
             testCollection()
         }
+
+        package_method_collection.setOnClickListener {
+            testCollection()
+        }
+
+        package_method_coroutine.setOnClickListener {
+            testCoroutine()
+        }
+    }
+
+    private fun testCoroutine() {
+        // 在后台启动一个新的协程并继续
+        val coroutineStatus = GlobalScope.launch {
+            //执行在非主线程
+            Log.d(TAG, "coroutine start!")
+            //非阻塞的等待 1 秒钟（默认时间单位是毫秒）
+            delay(5000)
+            Log.d(TAG, "coroutine finish!")
+
+        }
+        Log.d(TAG, "coroutine main thread!")
+
+
+        runBlocking {
+            //执行在主线程
+            Log.d(TAG, "coroutine2 start!")
+            //非阻塞的等待 10 秒钟（默认时间单位是毫秒），并不会ANR
+            delay(10000)
+            Log.d(TAG, "coroutine2 finish!")
+        }
+        Log.d(TAG, "coroutine2 main thread!")
     }
 }
 
