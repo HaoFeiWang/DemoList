bd280cd4520ca8fa9ef7dad30e71b1437f016105
wanghaofei <292265806@qq.com>
2019-07-16 08:25:58 +0800

    MVP增加动态代理

--- app/src/main/java/com/whf/demolist/mvp/MvpBasePresent.java
+++ app/src/main/java/com/whf/demolist/mvp/MvpBasePresent.java
@@ -1,23 +1,46 @@
 package com.whf.demolist.mvp;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * Created by WHF on 2017/11/5.
  */
 
-public abstract class MvpBasePresent<M extends IModel,V extends IView>{
+public abstract class MvpBasePresent<M extends IModel, V extends IView> {
 
     protected V view;
     protected M model;
+    private V proxyView;
 
-    protected MvpBasePresent(){
+    protected MvpBasePresent() {
         this.model = createModel();
+        this.createViewProxy();
     }
 
-    protected void attachView(V view){
+    @SuppressWarnings("unchecked")
+    private void createViewProxy() {
+        proxyView = (V) Proxy.newProxyInstance(view.getClass().getClassLoader(), view.getClass().getInterfaces(), new InvocationHandler() {
+            @Override
+            public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+                if (view == null) {
+                    return null;
+                }
+                return method.invoke(view, objects);
+            }
+        });
+    }
+
+    private V getView(){
+        return proxyView;
+    }
+
+    protected void attachView(V view) {
         this.view = view;
     }
 
-    protected void detachView(){
+    protected void detachView() {
         this.view = null;
     }
 
